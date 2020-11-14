87cef6fc2dd16627dfd224aa55ecf522c62792bc
maximetokman <maximetokman@gmail.com>
2020-11-13 21:35:46 -0800

    added aws amplify api

---
+++ bizi/src/graphql/mutations.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createTodo = `mutation CreateTodo($input: CreateTodoInput!) {
+  createTodo(input: $input) {
+    id
+    username
+    password
+  }
+}
+`;
+export const updateTodo = `mutation UpdateTodo($input: UpdateTodoInput!) {
+  updateTodo(input: $input) {
+    id
+    username
+    password
+  }
+}
+`;
+export const deleteTodo = `mutation DeleteTodo($input: DeleteTodoInput!) {
+  deleteTodo(input: $input) {
+    id
+    username
+    password
+  }
+}
+`;
---
+++ bizi/src/graphql/subscriptions.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = `subscription OnCreateTodo {
+  onCreateTodo {
+    id
+    username
+    password
+  }
+}
+`;
+export const onUpdateTodo = `subscription OnUpdateTodo {
+  onUpdateTodo {
+    id
+    username
+    password
+  }
+}
+`;
+export const onDeleteTodo = `subscription OnDeleteTodo {
+  onDeleteTodo {
+    id
+    username
+    password
+  }
+}
+`;
