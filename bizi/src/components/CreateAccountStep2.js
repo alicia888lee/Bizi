557524369eae85ed39cc9c9038ee4b2d85dd26bd
maximetokman <maximetokman@gmail.com>
2020-11-14 18:30:07 -0800

    working on step 2 of create

--- bizi/src/components/CreateAccountStep2.js
+++ bizi/src/components/CreateAccountStep2.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
+import Nav from './Nav'
 
 class Step2 extends Component {
-    
-
     render() {
         const { next } = this.props;
         return (
-            <div>
-                <button onClick={next}>Go to step3</button>
+            <div className='createAccountStep1'>
+                <Nav light={false} />
+                <div className='createAccountHeader'>
+                    <h1>Start Making Change!</h1>
+                </div>
+                <div className='createNextButton'>
+                    <button onClick={next}>Create</button>
+                </div>
+                <div className="circles">
+                    <div className="circleCreateAcct"></div>
+                    <div className="circleCreateAcct blueCircle"></div>
+                    <div className="circleCreateAcct"></div>
+                </div>
+                <div className='termsAgreement'>
+                    <a className="smallText" href="#">Terms of Agreement</a>   
+                </div>
             </div>
         )
     }
