847bb4698489be9a6baf3de4757a7d3853dda448
jeffersonxu <me@jeffersonxu.com>
2020-12-27 22:26:43 -0500

    added conditional flow to AddReview

--- bizi/src/components/AddReview.js
+++ bizi/src/components/AddReview.js
@@ -1,22 +1,62 @@
 import React from 'react'
 import { IoMdText, IoIosAdd } from "react-icons/io";
+import { Link } from "react-router-dom"
+import { Auth } from 'aws-amplify'
 
-function AddReview() {
-    return(
-        <div className="add-review-wrapper">            
-            <h2 className="add-review-header"><IoMdText className="review-icon"/> Leave a Review</h2>
-            <div className="review-wrapper">
-                <div className="review-upload">
-                    <h3>Upload Photo</h3>
-                    <IoIosAdd className="review-add-icon" />
+class AddReview extends React.Component {
+    constructor(props) {
+        super(props)  
+        this.state = {
+            userAuthenticated: false,
+        }
+    }
+    
+    // check if user is signed in
+    checkAuth = async() => {
+        try {
+            const currentUser = await Auth.currentAuthenticatedUser();
+            return currentUser;
+        }
+        catch (e){
+          console.log('error checking authentication', e);
+        }        
+    }
+  
+    updateUserState = (isAuth = false) => {
+        this.setState({
+          userAuthenticated: isAuth
+        });
+    }
+  
+    async componentDidMount() {
+        const verifyAuth = await this.checkAuth();                
+        verifyAuth && this.updateUserState(true);
+    }
+
+    render(){
+        const { userAuthenticated } = this.state;
+
+        if(userAuthenticated){
+            return( 
+                <div className="add-review-wrapper">            
+                    <h2 className="add-review-header"><IoMdText className="review-icon"/> Leave a Review</h2>
+                    <div className="review-wrapper">
+                        <div className="review-upload">
+                            <h3>Upload Photo</h3>
+                            <IoIosAdd className="review-add-icon" />
+                        </div>
+                        <div className="review-description">
+                            <textarea rows="4" className="reviewText" placeholder="Write a review..."/>
+                        </div>
+                    </div>
+                    <button className="reviewBtn">Post</button>
                 </div>
-                <div className="review-description">
-                    <textarea rows="4" className="reviewText" placeholder="Write a review..."/>
-                </div>
-            </div>
-            <button className="reviewBtn">Post</button>
-        </div>
-    )
+            )
+        }
+        return(            
+            <h2 className="reviewLogin"><Link to="/login">Login</Link> to your account leave a review!</h2>        
+        )
+    }
 }
 
 export default AddReview
