f487ae59f2ac75522535b89470bf3b71e75e921a
maximetokman <maximetokman@gmail.com>
2021-02-18 00:06:39 -0800

    implemented update business description

--- bizi/src/components/AccountBusiness.js
+++ bizi/src/components/AccountBusiness.js
@@ -1,13 +1,16 @@
 import React from "react"
 import { BsBookmarkPlus  } from "react-icons/bs";
 import { GiHealthNormal } from "react-icons/gi";
+import { BsPencil } from "react-icons/bs";
 import { IoIosAdd } from "react-icons/io";
 import { BiCalendarPlus, BiPhone } from "react-icons/bi";
 import { AiFillCamera } from "react-icons/ai";
 import { FiThumbsUp, FiThumbsDown  } from "react-icons/fi";
 import { RiFlag2Line } from "react-icons/ri";
-import * as queries from '../graphql/queries'
-import { API } from 'aws-amplify'
+import * as queries from '../graphql/queries';
+import * as mutations from '../graphql/mutations';
+import { API } from 'aws-amplify';
+import Loader from 'react-loader-spinner';
 
 class AccountBusiness extends React.Component {
     constructor(props){
@@ -15,7 +18,11 @@ class AccountBusiness extends React.Component {
         this.state = {
             loading: false,
             business: null,      
-            reviews: []       
+            reviews: [],
+            editDescription: false,
+            newBusinessDescription: "",
+            validBusinessDescription: true,
+            updatingBusinessDescription: false
         }
     }
 
@@ -31,13 +38,64 @@ class AccountBusiness extends React.Component {
             let business = listBusinesses.filter(item => item.userEmail === this.props.authUser.attributes.email)[0];                  
             this.setState({
                 business: business,     
-                loading: false           
+                loading: false,
+                newBusinessDescription: business?.businessDescription           
             });
         }
         catch (error) {
             console.log(error);
         }
     }
+
+    editDescription = async(action) => {
+        const { 
+            newBusinessDescription, 
+            business, 
+            validBusinessDescription,
+            updatingBusinessDescription
+        } = this.state;
+        switch(action) {
+            case "make-editable":
+                // create input box
+                this.setState({
+                    editDescription: true
+                });
+                break;
+            case "save":
+                // validate input
+                if (newBusinessDescription && validBusinessDescription) {
+                    // save
+                    this.setState({updatingBusinessDescription: true});
+                    console.log(newBusinessDescription);
+                    var updatedBusiness = {
+                        ...business,
+                        businessDescription: newBusinessDescription
+                    }
+                    try {
+                        await API.graphql({
+                            query: mutations.updateBusiness,
+                            variables: {input: updatedBusiness}
+                        });
+                    }
+                    catch(e) {
+                        console.log(e);
+                    }
+                    await this.getBusinessData();
+                    this.setState({
+                        editDescription: false,
+                        updatingBusinessDescription: false
+                    });
+                }
+                else {
+                    console.log("invalid desc");
+                    this.setState({validBusinessDescription: false});
+                }
+                
+            default:
+                break;
+        } 
+    }
+
 
     async componentDidMount() {
         await this.getBusinessData();    
@@ -66,23 +124,47 @@ class AccountBusiness extends React.Component {
         }
     }
 
+    componentDidUpdate(prevProps, prevState) {
+        const { newBusinessDescription } = this.state;
+        if (prevState.newBusinessDescription != newBusinessDescription) {
+            this.setState({validBusinessDescription: newBusinessDescription})
+        }
+    }
+
     render(){
         const { authUser } = this.props;       
-        const { business, reviews } = this.state;  
-        let policies = this.state.business?.policyList.map(item => <li>{item}</li>)    
-        let firstName = authUser?.attributes?.name.split(' ')[0]                                             
-                
+        const { 
+            business, 
+            reviews, 
+            editDescription,
+            validBusinessDescription,
+            updatingBusinessDescription
+        } = this.state;  
+        let policies = this.state.business?.policyList.map(item => <li>{item}</li>)
+        let firstName = authUser?.attributes?.name.split(' ')[0]
         return(
             <div className="account business-account">
                 <h1 className="accountHeader">Hey {firstName}! Welcome Back!</h1>                
-                <button className="interview-btn">Schedule your business spotlight interview</button>                            
+                <a href="https://calendly.com/biziinterview/30min" target="_blank"><button className="interview-btn" id="interview-schedule">Schedule your business spotlight interview</button></a>
                 <h3>Update your Business Information</h3>
 
                 <div className="business-info">
                     <h2><BsBookmarkPlus className="account-header-icon" /> { business?.businessName }</h2>
                     
                     <div className="business-info-wrapper">
-                        <p>{ business?.businessDescription }</p>
+                        {editDescription ? 
+                            <div className="loginBody">
+                                <input
+                                id={!validBusinessDescription && "invalidInput"}
+                                    type="text"
+                                    disabled={updatingBusinessDescription}
+                                    defaultValue={business?.businessDescription}
+                                    onChange={(e) => this.setState({newBusinessDescription: e.target.value})}
+                                />
+                                <button id="save-update" onClick={() => this.editDescription("save")}>Save</button>
+                                {updatingBusinessDescription && <Loader type='TailSpin' color='#385FDC' height={30}/>}
+                            </div> :
+                            <p>{ business?.businessDescription + "\t"}<BsPencil onClick={() => this.editDescription("make-editable")} id="edit-business"/></p>}
                         <div className="account-policies">
                             <GiHealthNormal className="business-account-icon" />
                             <ul>
