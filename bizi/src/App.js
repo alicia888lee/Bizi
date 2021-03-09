6ab37f6478f8139dd4397750d8fc8023d3d4db68
maximetokman <maximetokman@gmail.com>
2021-03-08 21:12:45 -0800

    added mobile user guidance

--- bizi/src/App.js
+++ bizi/src/App.js
@@ -13,37 +13,46 @@ import ScrollToTop from './components/ScrollToTop.js';
 
 function App() {
   var UNDER_CONSTRUCTION = false;
+  var IS_MOBILE = /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)
   return (
     <div className="App">
-      {UNDER_CONSTRUCTION ?
-        <h1>Website is currently under construction, check back later.</h1> :
-      <Router>
-        <ScrollToTop />
-        <Switch>          
-          <Route exact path="/">                        
-            <Home />
-          </Route>
-          <Route path="/stories">
-            <Stories />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>      
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/account">
-            <Account />
-          </Route>
-          <Route path="/create">
-            <CreateAccount />
-          </Route>
-          <Route path="/search" component={Search} />
-          <Route path="/forgot-password">
-            <ForgotPassword />
-          </Route>
-        </Switch>       
-      </Router>}
+      {UNDER_CONSTRUCTION &&
+        <h1>Website is currently under construction, check back later.</h1>
+      }
+      {IS_MOBILE && !UNDER_CONSTRUCTION &&
+        <div id='is-mobile'>
+          <h2>Looks like you're trying to find us from your phone...<br />...we're still working on the mobile site, so please visit us from your computer for now!</h2>
+        </div>
+      }
+      {!IS_MOBILE && !UNDER_CONSTRUCTION &&
+        <Router>
+          <ScrollToTop />
+          <Switch>          
+            <Route exact path="/">                        
+              <Home />
+            </Route>
+            <Route path="/stories">
+              <Stories />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>      
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/account">
+              <Account />
+            </Route>
+            <Route path="/create">
+              <CreateAccount />
+            </Route>
+            <Route path="/search" component={Search} />
+            <Route path="/forgot-password">
+              <ForgotPassword />
+            </Route>
+          </Switch>       
+        </Router>
+      }
     </div>
   );
 }
