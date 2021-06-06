15d4e5f6647c84395f24a38243399bddd9db5320
Robert Obrochta <obrochtarobert13@gmail.com>
2021-06-06 14:42:03 -0500

    login mobile screen

--- bizi/src/components/Login.js
+++ bizi/src/components/Login.js
@@ -4,7 +4,12 @@ import { Link, withRouter } from 'react-router-dom'
 import { Auth, Hub } from 'aws-amplify'
 import Loader from 'react-loader-spinner'
 
+
+var IS_MOBILE = /Android|webOS|iPhone|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent);
+var width = window.innerWidth;
+
 class Login extends Component {
+    
     constructor(props) {
         super(props)
 
@@ -25,6 +30,7 @@ class Login extends Component {
             console.log('error checking auth', error);
         }
     }
+    
 
     setUserEmail = (e) => {
         this.setState({
@@ -101,73 +107,145 @@ class Login extends Component {
             loggingIn
         } = this.state;
 
+        if (width > 768) {
+            return (
+                <div className="login">
+                    {width < 768 && <h1>get out</h1>}
+                    <Nav light={false} />
+                    <div className="loginHeader">
+                        <h1>Welcome Back!</h1>
+                        <p>New to Bizi? <Link to="/create">Create an Account</Link></p>
+                    </div>
 
-        return (
-            <div className="login">
-                <Nav light={false} />
-                <div className="loginHeader">
-                    <h1>Welcome Back!</h1>
-                    <p>New to Bizi? <Link to="/create">Create an Account</Link></p>
-                </div>
+                    <div className="loginBody">
+                        <form onSubmit={async(e) => {
+                            const error = await this.doLogin(e);
+                            console.log(error);
+                            if (error) {
+                                this.setErrorMessage(error);
+                            }
+                        }}>
+                            <div className="inputGroup">                    
+                                <label className="loginLabel" for="email">E-mail</label>
+                                <input className="loginInput" type="text" name="email" onInput={this.setUserEmail}/>
+                            </div>
+                            
+                            
+                            <div className="inputGroup">                    
+                                <label className="loginLabel" for="password">Password</label>
+                                <input type="password" name="password" onInput={this.setUserPassword}/>
+                                <p>{errorMessage}</p>         
+                            </div>        
 
-                <div className="loginBody">
-                    <form onSubmit={async(e) => {
-                        const error = await this.doLogin(e);
-                        console.log(error);
-                        if (error) {
-                            this.setErrorMessage(error);
-                        }
-                    }}>
-                        <div className="inputGroup">                    
-                            <label className="loginLabel" for="email">E-mail</label>
-                            <input type="text" name="email" onInput={this.setUserEmail}/>
+                            {/* <div className="checkboxGroup">
+                                <label for="rememberMe">Remember Me</label>
+                                <input type="checkbox" name="rememberMe"/>                            
+                            </div> */}
+
+                            <div className='inputGroup'>
+                                <input className='loginBtn' type='submit' value='Log In'/>
+                            </div>
+                            {
+                                loggingIn && <Loader
+                                    type="TailSpin"
+                                    color="#385FDC"
+                                    height={40}
+                                />
+                            }
+
+                            {/* <button className="loginBtn" onClick={() => {this.doLogin();}}>Log In</button> */}
+                            <Link className="smallText" to="forgot-password">Forgot your password?</Link>
+                        </form>
+                        <div className="socialLogin">
+                            <button className="facebook" onClick={this.doFacebookLogin}>Log in with Facebook</button>
+                            <button className="google" onClick={this.doGoogleLogin}>Log in with {
+                                <>
+                                    <span id='google-blue'>G</span>
+                                    <span id='google-red'>o</span>
+                                    <span id='google-yellow'>o</span>
+                                    <span id='google-blue'>g</span>
+                                    <span id='google-green'>l</span>
+                                    <span id='google-red'>e</span>
+                                </>
+                            }</button>
                         </div>
                         
-                        <div className="inputGroup">                    
-                            <label className="loginLabel" for="password">Password</label>
-                            <input type="password" name="password" onInput={this.setUserPassword}/>
-                            <p>{errorMessage}</p>         
-                        </div>        
-
-                        {/* <div className="checkboxGroup">
-                            <label for="rememberMe">Remember Me</label>
-                            <input type="checkbox" name="rememberMe"/>                            
-                        </div> */}
-
-                        <div className='inputGroup'>
-                            <input className='loginBtn' type='submit' value='Log In'/>
-                        </div>
-                        {
-                            loggingIn && <Loader
-                                type="TailSpin"
-                                color="#385FDC"
-                                height={40}
-                            />
-                        }
-
-                        {/* <button className="loginBtn" onClick={() => {this.doLogin();}}>Log In</button> */}
-                        <Link className="smallText" to="forgot-password">Forgot your password?</Link>
-                    </form>
-                    <div className="socialLogin">
-                        <button className="facebook" onClick={this.doFacebookLogin}>Log in with Facebook</button>
-                        <button className="google" onClick={this.doGoogleLogin}>Log in with {
-                            <>
-                                <span id='google-blue'>G</span>
-                                <span id='google-red'>o</span>
-                                <span id='google-yellow'>o</span>
-                                <span id='google-blue'>g</span>
-                                <span id='google-green'>l</span>
-                                <span id='google-red'>e</span>
-                            </>
-                        }</button>
                     </div>
-                    
+                    <div className='termsAgreement'>
+                        <a className="smallText" href="https://www.termsfeed.com/live/cce55d58-2f48-4d9c-ab31-478dafcdca99" target='_blank'>Privacy Policy</a>   
+                    </div>                     
                 </div>
-                <div className='termsAgreement'>
-                    <a className="smallText" href="https://www.termsfeed.com/live/cce55d58-2f48-4d9c-ab31-478dafcdca99" target='_blank'>Privacy Policy</a>   
-                </div>                     
-            </div>
-        );
+            );
+        } 
+
+        else if (width <= 768){
+            return (
+            <div className="login">
+                    <Nav light={false} />
+                    <div className="loginHeader">
+                        <h1>Welcome Back!</h1>
+                        <p>New to Bizi? <Link to="/create">Create an Account</Link></p>
+                    </div>
+
+                    <div className="loginBody">
+                        <form onSubmit={async(e) => {
+                            const error = await this.doLogin(e);
+                            console.log(error);
+                            if (error) {
+                                this.setErrorMessage(error);
+                            }
+                        }}>
+
+                            <div className="socialLogin">
+                                <button className="facebook" onClick={this.doFacebookLogin}>Log in with Facebook</button>
+                                <button className="google" onClick={this.doGoogleLogin}>Log in with {
+                                    <>
+                                        <span id='google-blue'>G</span>
+                                        <span id='google-red'>o</span>
+                                        <span id='google-yellow'>o</span>
+                                        <span id='google-blue'>g</span>
+                                        <span id='google-green'>l</span>
+                                        <span id='google-red'>e</span>
+                                    </>
+                                }</button>
+                            </div>
+
+                            <div className="horizLine">
+                                <hr></hr>
+                                <h3 className="midtext">OR</h3>
+                                <hr></hr>
+                            </div>
+
+                            <div className="inputGroup">                    
+                                <input className="loginInput" placeholder="E-mail" type="text" name="email" onInput={this.setUserEmail}/>
+                            </div>
+
+                            <div className="inputGroup">                    
+                                <input type="password" placeholder="Password" name="password" onInput={this.setUserPassword}/>
+                                <p>{errorMessage}</p>         
+                            </div>       
+
+                            <div className='inputGroup'>
+                                <input className='loginBtn' type='submit' value='Log In'/>
+                            </div>
+                            {
+                                loggingIn && <Loader
+                                    type="TailSpin"
+                                    color="#385FDC"
+                                    height={40}
+                                />
+                            }
+                            <Link className="smallText" to="forgot-password">Forgot your password?</Link>
+                        </form>
+                        
+                        </div>
+                        <div className='termsAgreement'>
+                            <a className="smallText" href="https://www.termsfeed.com/live/cce55d58-2f48-4d9c-ab31-478dafcdca99" target='_blank'>Privacy Policy</a>   
+                        </div>                     
+                    </div>
+            );
+        }
+
     }
 }
 
